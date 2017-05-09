704f074302365d750c117378a44dff41907eae8c
Satyam Sahu <satyamss2497@gmail.com>
2017-05-09 12:13:30 +0530

    First commit

---
+++ myapp/views/Controls.js
@@ -0,0 +1,69 @@
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('AccordionDemoCtrl', function ($scope) {
+    $scope.groups = [
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        },
+
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        },
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        },
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        },
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        }
+    ];
+    $scope.dynamicPopover = {
+        content: 'Hello, World!',
+        templateUrl: 'myPopoverTemplate.html',
+        title: 'Share via '
+    };
+
+    $scope.placement = {
+        options: [
+            'top',
+            'top-left',
+            'top-right',
+            'bottom',
+            'bottom-left',
+            'bottom-right',
+            'left',
+            'left-top',
+            'left-bottom',
+            'right',
+            'right-top',
+            'right-bottom'
+        ],
+        selected: 'top'
+    };
+});
---
+++ myapp/views/LoginController.js
@@ -0,0 +1,18 @@
+angular.module('ui.bootstrap.demo', ['$scope', '$rootScope', '$location', 'AuthenticationService']);
+angular.module('ui.bootstrap.demo').controller('LoginController', function ($scope, $rootScope, $location, AuthenticationService) {
+                // reset login status
+                AuthenticationService.ClearCredentials();
+
+                $scope.login = function () {
+                    $scope.dataLoading = true;
+                    AuthenticationService.Login($scope.username, $scope.password, function(response) {
+                        if(response.success) {
+                            AuthenticationService.SetCredentials($scope.username, $scope.password);
+                            $location.path('/');
+                        } else {
+                            $scope.error = response.message;
+                            $scope.dataLoading = false;
+                        }
+                    });
+                };
+});
